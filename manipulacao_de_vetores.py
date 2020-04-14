c0c9c711e29be3d9b1e584d986291961a91d3dc1
nataliabf <53270236+nataliabf@users.noreply.github.com>
2020-04-14 20:59:56 -0300

    Adição da impressão por ordem crescente

--- manipulacao_de_vetores.py
+++ manipulacao_de_vetores.py
@@ -20,7 +20,27 @@ def imprimir_elementos_ordem_inversa(vetor):
     if len(v) > 0:
         imprimir_elementos_ordem_inversa(v)
 
+
+def ordenar(vetor):
+    v = vetor.copy()
+    for i in range(len(v)):
+        for j in range(len(v) - 1):
+            if v[j] > v[j + 1]:
+                v[j], v[j + 1] = v[j + 1], v[j]
+    return v
+
+
+def imprimir_elementos_ordem_crescente(vetor):
+    v = ordenar(vetor)
+    print(v[0])
+    v.pop(0)
+    if len(v) > 0:
+        imprimir_elementos(v)
+
+
 print()
 imprimir_elementos(vetor1)
 print()
 imprimir_elementos_ordem_inversa(vetor1)
+print()
+imprimir_elementos_ordem_crescente(vetor1)
