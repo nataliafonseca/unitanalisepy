caba657c5e16abeb2d6c58225ad24d4fb38bc8a6
nataliabf <53270236+nataliabf@users.noreply.github.com>
2020-04-23 04:06:42 -0300

    Soma em vetores

--- soma_em_vetores.py
+++ soma_em_vetores.py
@@ -1,6 +1,6 @@
-def soma_em_vetores(vetor_exemplo, valor_buscado):
-    for indice_a, valor_a in enumerate(vetor_exemplo):
-        for indice_b, valor_b in enumerate(vetor_exemplo):
+def soma_em_vetores(vetor, valor_buscado):
+    for indice_a, valor_a in enumerate(vetor):
+        for indice_b, valor_b in enumerate(vetor):
             if valor_a != valor_b and valor_a + valor_b == valor_buscado:
                 return [indice_a, indice_b]
 
